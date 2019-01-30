63264809318501e06fd0f63b2bd48833c3617807
Mendelrock <lamiethno@gmail.com>
2019-01-30 15:47:23 +0100

    initial

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+var admin = require('firebase-admin');
+
+var serviceAccount = require('./system/serviceAccountKey.json');
+
+var app = express();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://food-for-fun-bdd.firebaseio.com/'
+});
